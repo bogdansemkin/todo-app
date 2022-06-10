3ae984cf9602266deb2c0b635f6436f8f6e2d5a7
Bogdan Donchenko <40574816+goddamityouscrewedupagain@users.noreply.github.com>
2022-06-10 12:07:57 +0300

    create repo 'n' services

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"github.com/spf13/viper"
 	"log"
 	todo_app "todo-app"
 	"todo-app/pkg/handler"
+	"todo-app/pkg/repository"
+	"todo-app/pkg/service"
 )
 
 func main(){
+	if err := initConfig(); err != nil{
+		log.Fatalf("error initializating configs: %s", err.Error())
+	}
+
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo_app.Server)
-	handlers := new(handler.Handler)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil{
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil{
 		log.Fatalf("error occured while running http server %d", err.Error())
 	}
+}
+
+func initConfig() error{
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
